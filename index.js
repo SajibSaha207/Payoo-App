a56c9b28f56a7c248115bfbedf857c7e18aabf9c
SajibSaha207 <sajibsaha.207@gmail.com>
2025-09-10 23:47:38 +0600

    Added home page

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
 
 document.getElementById("login-btn")
 .addEventListener("click", function(){
- 
+    event.preventDefault(); 
+    
+    const mobileNumber = 12345678910
+    const pinNumber = 1234
+    const mobileNumeberValue = document.getElementById("mobile-number" ).value 
+    const mobileNumeberValueConverted = parseInt (mobileNumeberValue)
+    const pinNumeberValue = document.getElementById("pin-number" ).value 
+    const pinNumeberValueConverted = parseInt (pinNumeberValue)
+
+//  console.log(mobileNumeberValueConverted, pinNumeberValueConverted)
+if (mobileNumeberValueConverted === mobileNumber && pinNumeberValueConverted === pinNumber){
+window.location.href="./home.html"
+}
+else{
+    alert("Invalid Number or Pin !!")
+}
 })
